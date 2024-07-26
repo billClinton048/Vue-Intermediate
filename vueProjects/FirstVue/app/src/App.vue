83db723dc458202c15a555a2af3bc340e8ba567b
billClinton048 <clintonkaranja46@gmail.com>
2024-07-26 03:14:43 +0300

    message

---
+++ vueProjects/FirstVue/app/src/App.vue
@@ -0,0 +1,16 @@
+  <div class="min-h-screen bg-white">
+    <Header />
+    <div class="mt-4">
+      <RouterView />
+    </div>
+    <Footer />
+  </div>
+</template>
+
+<script setup>
+import { RouterView } from "vue-router";
+import Header from "./components/Header.vue";
+import Footer from "./components/Footer.vue";
+</script>
+
+<style scoped></style>
